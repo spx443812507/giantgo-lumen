a9acfb8e8ee371565e97a49a8ecd33dee47640d3
siler <spx123456@foxmail.com>
2017-07-04 10:24:24 +0800

    dynamic env config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    (new Dotenv\Dotenv(__DIR__ . '/../'))->load();
+    (new Dotenv\Dotenv(__DIR__ . '/../', $_SERVER['SERVER_NAME'] . '.env'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
