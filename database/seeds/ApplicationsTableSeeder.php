4441b0a4c2f1de1f43d2de737fdcd1b72f1a8386
siler_shen <spx123456@foxmail.com>
2019-03-20 16:56:47 +0800

    clean code

--- database/seeds/ApplicationsTableSeeder.php
+++ database/seeds/ApplicationsTableSeeder.php
@@ -13,20 +13,20 @@ class ApplicationsTableSeeder extends Seeder
     {
         $application = [
             [
-                'client_id' => '101405226',
-                'client_secret' => '22350fe680bec6a854f715fe149ec747',
+                'client_id' => '',
+                'client_secret' => '',
                 'redirect' => '/oauth/qq/callback',
                 'provider' => 'qq'
             ],
             [
-                'client_id' => 'wx8f6f554a22bb28db',
-                'client_secret' => '4681fc6e3ff1c1530544761e57562922',
+                'client_id' => '',
+                'client_secret' => '',
                 'redirect' => '/oauth/wechat/callback',
                 'provider' => 'wechat'
             ],
             [
-                'client_id' => 'wx414a6131135563d7',
-                'client_secret' => '9e7c854999c9805b8fd83d8d52c790be',
+                'client_id' => '',
+                'client_secret' => '',
                 'redirect' => '/oauth/wechat_web/callback',
                 'provider' => 'wechat_web'
             ]
